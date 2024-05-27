9aed7bb13d166bd74e8b3b9c5d908a199d878fbe
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-05-27 11:03:01 +0000

    Aora project folder structure setup

--- app/screens/Onboarding.jsx
+++ app/screens/Onboarding.jsx
@@ -1,13 +1,22 @@
 // Onboarding.jsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Onboarding = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>Onboarding Screen</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#161622',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Onboarding;
